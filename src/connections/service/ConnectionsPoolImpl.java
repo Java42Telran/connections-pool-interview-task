753deb69d0f9e3bf63bf4f1ca2c3d0e1593f230f
Yuragranovsky <yuragranovsky@gmail.com>
2022-02-02 17:13:10 +0200

    hw #34 solution

--- src/connections/service/ConnectionsPoolImpl.java
+++ src/connections/service/ConnectionsPoolImpl.java
@@ -9,10 +9,14 @@ public class ConnectionsPoolImpl implements ConnectionsPool {
 		 Connection connection;
 		 Node prev;
 		 Node next;
+		 Node(Connection connection) {
+			this.connection = connection;
+		}
 	}
 	private static class ConnectionsList {
 		Node head;
 		Node tail;
+		
 	}
 	ConnectionsList list = new ConnectionsList();
 	HashMap<Integer, Node> mapConnections = new HashMap<>();
@@ -23,14 +27,76 @@ public class ConnectionsPoolImpl implements ConnectionsPool {
 
 	@Override
 	public boolean addConnection(Connection connection) {
-		// TODO Auto-generated method stub
-		return false;
+		if(mapConnections.containsKey(connection.getId())) {
+			return false;
+		}
+		Node node = new Node(connection);
+		addHead(node);
+		if (connectionsPoolLimit == mapConnections.size()) {
+			mapConnections.remove(list.tail.connection.getId());
+			removeTail();
+			
+		}
+		mapConnections.put(connection.getId(), node);
+		
+		return true;
+		
+	}
+
+	private void removeTail() {
+		Node newTail = list.tail.prev;
+		newTail.next = null;
+		list.tail = newTail;
+		
+		
+	}
+
+	private void addHead(Node node) {
+		
+		
+		if(list.tail == null) {
+			list.head = list.tail = node;
+		} else {
+			moveToHead(node);
+		}
+		
+		
+		
+		
+	}
+
+	private void moveToHead(Node node) {
+		node.next = list.head;
+		node.prev = null;
+		list.head.prev = node;
+		list.head = node;
 	}
 
 	@Override
 	public Connection getConnection(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Node node = mapConnections.get(id);
+		if (node == null) {
+			return null;
+		}
+		Connection connection = node.connection;
+		if (list.head != node) {
+			removeNode(node);
+			moveToHead(node);
+		}
+		return connection;
+	}
+
+	private void removeNode(Node node) {
+		if (list.tail == node) {
+			removeTail();
+		} else {
+			Node prev = node.prev;
+			Node next = node.next;
+			prev.next = next;
+			next.prev = prev;
+			
+		}
+		
 	}
 
 }
--- src/connections/service/ConnectionsPoolTest.java
+++ src/connections/service/ConnectionsPoolTest.java
@@ -5,20 +5,39 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ConnectionsPoolTest {
+import connections.dto.Connection;
 
+class ConnectionsPoolTest {
+	int limit = 5;
+ConnectionsPool pool;
 	@BeforeEach
 	void setUp() throws Exception {
+		pool = new ConnectionsPoolImpl(limit);
+		for (int i = 1; i <= limit; i++) {
+			pool.addConnection(new Connection(i, "address", 8080));
+		}
 	}
 
 	@Test
 	void testAddConnection() {
-		fail("Not yet implemented");
+		int id = limit + 1;
+		Connection connection = new Connection(id, "address", 8080);
+		assertTrue(pool.addConnection(connection));
+		assertFalse(pool.addConnection(connection));
+		assertNull(pool.getConnection(1));
+		assertNotNull(pool.getConnection(id));
+		
 	}
 
 	@Test
 	void testGetConnection() {
-		fail("Not yet implemented");
+		assertNotNull(pool.getConnection(1));
+		for(int i = limit + 1; i < limit * 2; i++) {
+			pool.addConnection(new Connection(i, "bbbbb", 10));
+		}
+		assertNull(pool.getConnection(limit));
+		assertNotNull(pool.getConnection(1));
 	}
+	
 
 }
